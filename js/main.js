61bb6d5ba88e97edbf2f243254f2ec2defcc736e
David Muroň <it2517@sspu-opava.cz>
2026-01-14 12:34:27 +0100

    zmena hlavniho indexu.html

--- js/main.js
+++ js/main.js
@@ -1,9 +1,8 @@
-<script>
-    function myFunction()
-        var x = document.getElementById("myTopnav");
-        if (x.className === "topnav") {
-            x.className += " responsive"
-        } else {
-            x.className = "topnav"
-        }
-</script>
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive"
+    } else {
+        x.className = "topnav"
+    }
+}
